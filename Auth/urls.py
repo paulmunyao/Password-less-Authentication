6a84fb5c26882e56eb98ef85d9d3b37323d03c8a
paulmunyao <paulmunyao094@gmail.com>
2022-08-17 12:53:27 +0300

    new code line

--- Auth/urls.py
+++ Auth/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AUthenticate', include('AUthenticate.urls')),
+    path('', include('AUthenticate.urls')),
 ]

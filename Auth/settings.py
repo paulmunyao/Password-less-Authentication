4331116ee3cc1f6862e061eb0576a035b7278685
paulmunyao <paulmunyao094@gmail.com>
2022-08-18 05:32:19 +0300

    edit library

--- Auth/settings.py
+++ Auth/settings.py
@@ -51,11 +51,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    ...
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        ...
+   
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    
     ...
 }
 

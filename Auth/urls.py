74e1ec1bb8125ad2bf2411c7560ef19d05bba351
paulmunyao <paulmunyao094@gmail.com>
2022-08-18 12:34:07 +0300

    remove unused code

--- Auth/urls.py
+++ Auth/urls.py
@@ -24,10 +24,7 @@ from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AUthenticate.urls')),
-    # path('auth/', include('djoser.urls')),
     path('api/v1', include('djoser.urls')),
     path('api/v1', include('djoser.urls.jwt')),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
 ]

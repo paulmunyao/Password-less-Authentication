7867b6896f90517f8a1bdc770b8daea9f98c2660
paulmunyao <paulmunyao094@gmail.com>
2022-08-18 08:49:56 +0300

    edit url path

--- Auth/urls.py
+++ Auth/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AUthenticate.urls')),
     path('auth/', include('djoser.urls')),
-    path(r'^auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.jwt')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),

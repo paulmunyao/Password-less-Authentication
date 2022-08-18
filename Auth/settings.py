500ffac826cc817b72dbaf894c40f193fc0471e9
paulmunyao <paulmunyao094@gmail.com>
2022-08-18 06:01:43 +0300

    edit code

--- Auth/settings.py
+++ Auth/settings.py
@@ -51,10 +51,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# REST_FRAMEWORK = {
-#     'rest_framework_simplejwt.authentication.JWTAuthentication',
-# }
-
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': (

dce2be5b4afff47e996f7f2e14b58e2e3ca12c49
paulmunyao <paulmunyao094@gmail.com>
2022-08-18 12:39:06 +0300

    remove unused code

--- Auth/urls.py
+++ Auth/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from rest_framework_simplejwt.views import TokenVerifyView
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+# from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),

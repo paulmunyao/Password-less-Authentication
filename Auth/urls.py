8ffed390d118dacf853963987798ed6baec1b5f0
paulmunyao <paulmunyao094@gmail.com>
2022-09-08 23:40:49 +0300

    remove imported simplejwt views

--- Auth/urls.py
+++ Auth/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [

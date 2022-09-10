9dcaff94cd660b45875b9361b4a8971aa452ea7a
paulmunyao <paulmunyao094@gmail.com>
2022-09-10 22:42:59 +0300

    indent code

--- Auth/urls.py
+++ Auth/urls.py
@@ -24,7 +24,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AUthenticate.urls')),
-   
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),

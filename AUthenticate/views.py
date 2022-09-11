90471518c7bae1166abe082d8e3911faed122d22
paulmunyao <paulmunyao094@gmail.com>
2022-09-11 23:05:20 +0300

    add user model to views

--- AUthenticate/views.py
+++ AUthenticate/views.py
@@ -14,7 +14,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username')
 
 
+        model = get_user_model()
+        fields = ('id', 'username')
 
+
+class UserAPIView(RetrieveAPIView):
 
 
 

0a43e635580a82826db870b0d089e50fa93e2555
paulmunyao <paulmunyao094@gmail.com>
2022-09-11 23:06:42 +0300

    indent code

--- AUthenticate/views.py
+++ AUthenticate/views.py
@@ -6,13 +6,10 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
 
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('id', 'username')
-
 
 
 

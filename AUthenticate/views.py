c1939d7c77b33e85f1f2f4307c86a6f40b4e5191
paulmunyao <paulmunyao094@gmail.com>
2022-09-11 23:05:01 +0300

    add user model to views

--- AUthenticate/views.py
+++ AUthenticate/views.py
@@ -1,4 +1,20 @@
 
+from django.contrib.auth import get_user_model
+
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import serializers
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
+
+
+
 
 
 

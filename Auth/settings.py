20b0fed76601697595ea5c8ffe2874ee4ddcde40
paulmunyao <paulmunyao094@gmail.com>
2022-09-12 03:28:47 +0300

    indent code

--- Auth/settings.py
+++ Auth/settings.py
@@ -57,9 +57,9 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),'DEFAULT_PERMISSION_CLASSES': (
+    ), 'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser'
-   ),
+    ),
 }
 
 SIMPLE_JWT = {

dfb157b111b014bd461c85bf242c023cbd6391ac
paulmunyao <paulmunyao094@gmail.com>
2022-09-12 03:30:19 +0300

    return response import in views

--- AUthenticate/views.py
+++ AUthenticate/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.generics import RetrieveAPIView
-# from rest_framework.response import Response
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
 
